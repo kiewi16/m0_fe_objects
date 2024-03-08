dbd33e2be1d4aa75454c6da8d5d2f2c226529a08
Kim Ewing <pekar.kimberly@gmail.com>
2024-03-08 07:00:32 -0700

    Adds completed syntaxCleanUp exercise

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -2,7 +2,18 @@ var foods = {apples: 23,
   grapes: 507, 
   eggs: 48}
 
+  var foods = {
+    apples: 23, 
+    grapes: 507, 
+    eggs: 48
+  }
+
 var person = { height: "6 ft", weight: "160 lbs" }
+
+var person = {
+  height: "6 ft", 
+  weight: "160 lbs" 
+}
 
 var post = { imageSrc: "./images/beach.png",
 
@@ -11,6 +22,13 @@ var post = { imageSrc: "./images/beach.png",
         number_likes: 0,
 comments: [] }
 
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+}
 
 var post2 =
 {
@@ -20,3 +38,16 @@ var post2 =
       numberLikes: 13,
       comments: []
 }
+
+var post2 =
+{
+  imageSrc2: "./images/holiday-party.png",
+  caption2: "What a great holiday party omg",
+  timestamp2: "11:37 PM December 31, 2019",
+  numberLikes2: 13,
+  comments2: []
+}
+
+// If we use the key-value pairs above, these key-value pairs will overwright the previous ones
+// becase the same keys were used in var post. I updated the keys in var post2, so the values in
+// var post would not be overwritten. 
